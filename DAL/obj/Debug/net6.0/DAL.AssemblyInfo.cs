17d1d2b7f292fe0a94cfce3bf32142fef60e5cf0
Rei <rdurann@sonda.com>
2024-10-14 22:45:58 -0500

    Ultima correción cadena de conexión.

--- DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72de525f5150c431a5a606bd3a581222d9bb590b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b840b530a2bfb81fb47cc7cd463ea68bb26f09")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
